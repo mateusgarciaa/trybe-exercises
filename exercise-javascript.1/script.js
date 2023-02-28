5371c712c7208d5b51c2a11d4c366bc8b8c4cb53
Mateus Garcia <mateus.nazareth@hotmail.com>
2023-02-28 02:41:37 -0300

    Parte else/if e switch

--- exercise-javascript.1/script.js
+++ exercise-javascript.1/script.js
@@ -1,3 +1,5 @@
+//-----------------------------------------------Exercicios de Operadores------------------
+
 let a = 7;
 let b = 4;
 let c = 12;
@@ -42,6 +44,7 @@ if(number > 0){
 
 let anguloTriangulo = a + b + c;
 let todosAngulosPositivos = a > 0 && b > 0 && c >> 0;
+
 if(todosAngulosPositivos){
     if(anguloTriangulo === 180){
     console.log(true)
@@ -50,5 +53,69 @@ if(todosAngulosPositivos){
 }
 } else{
     console.log('Ângulo Inválido!')
+}
+
+//----------------------------------------Exercicios de switch/case -------------------------------
+
+
+let pecaXadrez = 'torre';
+
+switch(pecaXadrez){
+
+    case 'peão':
+       console.log('Peão -> Retas (limitados a uma casa por turno , podendo optar por andar duas na primeira rodada)');
+
+     break;
+
+    case 'cavalo':
+        console.log('Cavalo -> Movimentos em formatos de L');
+
+    break;
+
+     case 'torre' :
+        console.log('Torre -> Apenas em agulos de 90 graus sem limites de casas , não podendo passar por cima de nenhuma outra peça');
+    
+    break;
+
+    case 'rainha':
+        console.log('Rainha -> Qualquer direção e sem limite de quantidade de casas');
+
+    break;
+    
+    case 'bispo':
+        console.log('Bispo -> Apenas diagonais sem limite de quantidade de casas');
+
+    break;
+
+     case 'rei':
+        console.log('Rei -> Qulquer direção mas apenas uma casa por turno');
+     
+     default:
+        console.log('Erro, peça inválida');   
+
+}
+
+
+//----------------------------------------Exercicios else/if---------------------------------------
+
+
+
+let notaPorcentagem = 70;
+
+
+if(notaPorcentagem > 100 || notaPorcentagem < 0){
+    console.log('Erro na nota')
+}else if(notaPorcentagem >= 90 ){
+    console.log('Nota A');
+}else if(notaPorcentagem >=80){
+    console.log('Nota B');
+}else if(notaPorcentagem >=70){
+    console.log('Nota C');
+}else if(notaPorcentagem >=60){
+    console.log('Nota D');
+}else if(notaPorcentagem >=50){
+    console.log('Nota E');
+}else if(notaPorcentagem < 50){
+    console.log('Nota F');
 }
 
