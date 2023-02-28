14504cd7b1e6e0fe665c3f779472741425af5417
Mateus Garcia <mateus.nazareth@hotmail.com>
2023-02-28 15:03:41 -0300

    Parte final do exercicio

--- exercise-javascript.1/script.js
+++ exercise-javascript.1/script.js
@@ -119,3 +119,72 @@ if(notaPorcentagem > 100 || notaPorcentagem < 0){
     console.log('Nota F');
 }
 
+
+
+//----------------------------------------Exercicio par/impar --------------------------------
+
+
+
+let number1 = 43;
+let number2 = 32;
+let number3 = 27;
+let even = false;
+if ((number1 % 2 === 0 || number2 % 2 === 0 || number3 % 2 === 0)){
+    even = true;
+}
+console.log(even)
+
+
+let number4 = 43;
+let number5 = 32;
+let number6 = 27;
+let odd = false;
+if ((number1 % 2 !== 0 || number2 % 2 !== 0 || number3 % 2 !== 0)){
+    odd = true;
+}
+console.log(odd)
+
+
+//-----------------------------------------------Exercicio Lucro Total --------------------------
+
+let valorCusto = 1000;
+let valorVenda = 2150;
+let valorCustoTotal = valorCusto * 1.2;
+let quantidadeProdutos = 1000;
+let lucroTotal = (valorVenda - valorCustoTotal) * quantidadeProdutos;
+if(valorCusto < 0 || valorVenda < 0){
+    console.log("Erro no valor colocado")
+}else{
+    console.log(lucroTotal)
+}
+
+//----------------------------------------------Exercicio Salário -----------------------------
+
+let salarioBruto = 13250;
+let aliquotaINSS;
+let aliquotaIR;
+
+if(salarioBruto <= 1559.94){
+    aliquotaINSS = salarioBruto * 0.08
+}else if(salarioBruto <= 2594.92){
+    aliquotaINSS = salarioBruto * 0.09
+}else if(salarioBruto <= 5189.82){
+    aliquotaINSS = salarioBruto * 0.11
+}else{
+    aliquotaINSS = 570.88
+}
+
+let salarioBase = salarioBruto - aliquotaINSS;
+
+if (salarioBase < 1903.38){
+    aliquotaIR = 0
+}else if(salarioBase <= 2826.65){
+    aliquotaIR =(salarioBase * 0.075) - 142.80
+}else if(salarioBase <= 3751.05){
+    aliquotaIR = (salarioBase * 0.15) - 354.80
+}else if(salarioBase <= 4664.68){
+    aliquotaIR = (salarioBase * 0.225) - 636.13
+}else{
+    aliquotaIR = (salarioBase * 0.275) - 869.36
+}
+console.log('Salário Liquido é de' + " "+(salarioBase - aliquotaIR)+" "+ 'reais')
