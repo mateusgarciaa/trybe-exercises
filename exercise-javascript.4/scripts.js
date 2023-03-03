b90372b07544e7f4a3574701476b66c4066facb0
Mateus Garcia <mateus.nazareth@hotmail.com>
2023-03-03 17:38:22 -0300

    Finalizando o exercicio

--- exercise-javascript.4/scripts.js
+++ exercise-javascript.4/scripts.js
@@ -76,3 +76,79 @@ leitor.livrosFavoritos.push({
 //-------------------------------Informando a quantidade de livros favoritos -----------------
 
   console.log(`${leitor.nome} tem ${leitor.livrosFavoritos.length} livros favoritos`);
+
+
+  //------------------------------Exercicios pedido de clientes -------------------------------
+
+  let order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+      street: 'Rua das Flores',
+      number: '389',
+      apartment: '701',
+    },
+    order: {
+      pizza: {
+        marguerita: {
+          amount: 1,
+          price: 25,
+        },
+        pepperoni: {
+          amount: 1,
+          price: 20,
+        },
+      },
+      drinks: {
+        coke: {
+          type: 'Coca-Cola Zero',
+          price: 10,
+          amount: 1,
+        },
+      },
+      delivery: {
+        deliveryPerson: 'Ana Silveira',
+        price: 5,
+      },
+    },
+    payment: {
+      total: 60,
+    },
+  };
+  
+  function customerInfo(order) {
+    return `Olá, ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}.`;
+  }
+
+  console.log(customerInfo(order));
+
+  //----------------------------------------Modificando pedido -------------------------------
+
+  customerInfo(order);
+  
+  function orderModifier(order) {
+    order.name = 'Luiz Silva';
+    let pizzas = '';
+    let drinks = '';
+    let totalPrice = 0;
+  
+    for (let pizza in order.order.pizza) {
+      pizzas += `${pizza}, `;
+      totalPrice += order.order.pizza[pizza].price * order.order.pizza[pizza].amount;
+    }
+    pizzas = pizzas.slice(0, -2);
+  
+    for (let drink in order.order.drinks) {
+      drinks += `${order.order.drinks[drink].type}, `;
+      totalPrice += order.order.drinks[drink].price * order.order.drinks[drink].amount;
+    }
+    drinks = drinks.slice(0, -2);
+  
+    order.payment.total = totalPrice.toFixed(2);
+  
+    return `Olá, ${order.name}, o valor total de seu pedido de ${pizzas} e ${drinks} é R$ ${order.payment.total.replace('.', ',')}.`;
+  }
+  
+  console.log(orderModifier(order));
+  
+      
